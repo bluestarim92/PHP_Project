e2aedb696b543634f2dcacf296b9ae5235020dd8
bluestarim92 <bluestarim92@naver.com>
2021-07-09 13:01:36 +0900

    all file upload

---
+++ include/define.php
@@ -0,0 +1,10 @@
+<?php    
+	// define('DBhost','ashram777.cafe24.com');
+    // define('DBuser','ashram777');
+    // define('DBpass','dhanstjd77!');
+    // define('DBname','ashram777');
+	define('DBhost', 'localhost');
+	define('DBuser', 'bluestarim92');
+	define('DBpass', 'ay2111!!');
+	define('DBname', 'bluestarim92');
+?>
